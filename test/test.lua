47c040b459aada8e4d35e1e576ceb353f234bf7d
Lef Ioannidis <elefthei@mit.edu>
2017-10-18 19:05:22 -0400

    Use indirect non-blocking nginx requests for perf

--- test/test.lua
+++ test/test.lua
@@ -3,13 +3,14 @@ requests = require('requests')
 
 local webtest = torch.TestSuite()
 local tester = torch.Tester()
+local host = 'http://localhost:3000'
 
 function webtest.trainXOR()
   -- Define random input 2d tensor
   input = torch.rand(2)
   req_data = torch.serialize(input, 'ascii')
   req_headers = { ["Content-Type"] = "text/plain" }
-  resp = requests.post{url = 'http://localhost/impl/train', headers = req_headers, data = req_data}
+  resp = requests.post{url = host .. "/train", headers = req_headers, data = req_data}
   tens_response = torch.deserialize(resp.text, 'ascii')
   -- Replicate the XOR logic in src/train.lua
   local expected = torch.Tensor(1)
@@ -23,7 +24,7 @@ function webtest.trainXOR()
 end
 
 function webtest.outputXOR()
-  response = requests.get{url = 'http://localhost/impl/output'}
+  response = requests.get{url = host .. "/output"}
   -- XOR less than zero
   tester:assertlt(tonumber(response.text), 0, "XOR output of 0.5 ^ 0.5 should be a negative number")
 end
