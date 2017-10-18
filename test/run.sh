9605622c773a9ed4ad3516c202184d10f354210c
Lef Ioannidis <elefthei@mit.edu>
2017-10-18 19:25:44 -0400

    Remove cruft args, normalize to port 3000

--- test/run.sh
+++ test/run.sh
@@ -3,4 +3,5 @@ echo "============= Running WebTorch server"
 nginx
 echo "============= Running WebTorch server tests"
 luajit test/test.lua
-
+echo "============= WebTorch server tests finished, stopping server..."
+nginx -s stop
