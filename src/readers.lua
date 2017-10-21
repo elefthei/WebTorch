ed01db497c76ede83ba53f825e289c09907fd070
Lef Ioannidis <lef@logos.macbook.air>
2017-10-20 23:53:47 -0700

    Add image deserializer

--- src/readers.lua
+++ src/readers.lua
@@ -13,35 +13,10 @@ function deserializers.tensor(request_body)
   return file:readObject() -- gets a Tensor back (or any object sent)
 end
 
--- Read tensor from request body
--- Assumes input has been validated
-function deserializers.csv(request_body)
+function deserializers.image(request_body)
   local file = torch.MemoryFile() -- creates a file in memory
   file:writeString(data)
   file:seek(1) -- comes back at the beginning of the file
-  return file:readObject() -- gets a Tensor back (or any object sent)
+  return image.load(file, 3, 'byte') -- asume 3 channel (RGB) images
 end
-
-function deserializers.jpg(request_body)
-  local file = torch.MemoryFile() -- creates a file in memory
-  file:writeString(data)
-  file:seek(1) -- comes back at the beginning of the file
-  return file:readObject() -- gets a Tensor back (or any object sent)
-end
-
-function deserializers.png(request_body)
-  local file = torch.MemoryFile() -- creates a file in memory
-  file:writeString(request_body)
-  file:seek(1) -- comes back at the beginning of the file
-  return file:readObject() -- gets a Tensor back (or any object sent)
-end
-
-function deserializers.bmp(request_body)
-  local file = torch.MemoryFile() -- creates a file in memory
-  file:writeString(request_body)
-  file:seek(1) -- comes back at the beginning of the file
-  return file:readObject() -- gets a Tensor back (or any object sent)
-end
-
-
 
